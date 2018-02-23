958e8afe690e429de22661c941a994fabc2bb35a
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-02-23 17:58:49 -0300

    - Correção de erro

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -8,7 +8,7 @@ class ApiClient {
   }
 
   async getPlans () {
-    const response = await fetch(`https://evening-crag-52215.herokuapp.com/plans`)
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/plans`)
     return await response.json()
   }
 
@@ -18,7 +18,7 @@ class ApiClient {
       headers: this.headers,
       body: JSON.stringify(simulationPayload)
     }
-    const response = await  fetch(`https://evening-crag-52215.herokuapp.com/simulations`, payload)
+    const response = await  fetch(`${process.env.REACT_APP_API_BASE_URL}/simulations`, payload)
     return await response.json()
   }
 
@@ -28,7 +28,7 @@ class ApiClient {
       headers: this.headers,
       body: JSON.stringify(clientPayload)
     }
-    await  fetch(`https://evening-crag-52215.herokuapp.com/clients`, payload)
+    await  fetch(`${process.env.REACT_APP_API_BASE_URL}/clients`, payload)
   }
 }
 
